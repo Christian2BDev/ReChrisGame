9fe631bc055b299357587356caf2c2340a9ed07e
Christian2BDev <christiantenbrinke@hotmail.com>
2024-09-09 16:04:06 +0200

    rename items

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -5,12 +5,12 @@ public static class Inventory
 {
     private static int wood = 0;
     private static int stone = 0;
-    private static int food = 0;
+    private static int meat = 0;
     private static int gold = 0;
-    private static int iron = 0;
+    private static int steel = 0;
 
     public enum Materials {
-        wood,stone,food,iron,gold
+        wood,stone,meat,steel,gold
     }
 
     public static void ChangeItemAmount(Materials m, int a) {
@@ -18,9 +18,9 @@ public static class Inventory
         {
             case Materials.wood: wood += a; break;
             case Materials.stone: stone += a; break;
-            case Materials.food: food += a; break;
+            case Materials.meat: meat += a; break;
             case Materials.gold: gold += a; break;
-            case Materials.iron: iron += a; break;
+            case Materials.steel: steel += a; break;
         }
         UpdateItems();
        
@@ -30,9 +30,9 @@ public static class Inventory
         {
             case Materials.wood: return wood; 
             case Materials.stone:return stone; 
-            case Materials.food: return food; 
+            case Materials.meat: return meat; 
             case Materials.gold: return gold; 
-            case Materials.iron: return iron;
+            case Materials.steel: return steel;
             default: return -1;
         }
       
@@ -41,8 +41,8 @@ public static class Inventory
     private static void UpdateItems() {
         GameObject.Find("woodValue").GetComponent<TMP_Text>().text = wood.ToString();
         GameObject.Find("stoneValue").GetComponent<TMP_Text>().text = stone.ToString();
-        GameObject.Find("foodValue").GetComponent<TMP_Text>().text = food.ToString();
-        GameObject.Find("ironValue").GetComponent<TMP_Text>().text = iron.ToString();
+        GameObject.Find("meatValue").GetComponent<TMP_Text>().text = meat.ToString();
+        GameObject.Find("steelValue").GetComponent<TMP_Text>().text = steel.ToString();
         GameObject.Find("goldValue").GetComponent<TMP_Text>().text = gold.ToString();
     }
 }
