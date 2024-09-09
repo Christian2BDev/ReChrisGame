b9c1d4d8f38bb376d8d745966e1083d2b4fe9a5e
michiel2005 <60014996+Flawake@users.noreply.github.com>
2024-09-09 22:27:37 +0200

    Improved player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,37 +7,73 @@ public class PlayerController : MonoBehaviour
     public static GameObject playerReference;
 
     [SerializeField]
-    private float speed = 10f;
+    private float accelerationMultiplier = 0.01f;
+
+    [SerializeField]
+    private float decelerationMultiplier = 0.3f;
+
     [SerializeField]
     Rigidbody2D rb;
 
     [SerializeField]
     float smoothTime = 0.2f;
+
+    [SerializeField]
+    float maxAngularVelocity = 45f;
+
+    [SerializeField]
+    float maxVelocity = 1f;
+
+    float velocity = 0;
     // Start is called before the first frame update
     void Awake()
     {
         playerReference = this.gameObject;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (rb.velocity != Vector2.zero)
-        {
-            // Get the angle in degrees based on the velocity direction
-            float angle = Mathf.Atan2(rb.velocity.y, rb.velocity.x) * Mathf.Rad2Deg;
-
-            // Subtract 90 degrees to make the top of the sprite face the direction of movement
-            transform.rotation = Quaternion.Euler(0, 0, angle - 90);
-        }
-    }
-
     void FixedUpdate()
     {
         // Get target velocity based on player input
-        Vector2 targetVelocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized * speed;
+        Vector2 movementVec = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
+
+        if (movementVec == Vector2.zero) {
+            rb.angularVelocity = 0;
+            return;
+        }
+        float targetAngle = Mathf.Atan2(movementVec.y, movementVec.x) * Mathf.Rad2Deg;
+        targetAngle -= 90;
+
+        float curAngle = transform.eulerAngles.z;
+
+        float anglediff = Mathf.DeltaAngle(curAngle, targetAngle);
+
+        float desiredAngularVelocity = Mathf.Clamp(anglediff, -maxAngularVelocity, maxAngularVelocity);
+
+        rb.angularVelocity = desiredAngularVelocity;
+
+        //Vector2 curDirection = AngleToVector(curAngle + 90);
+        //Vector2 deltaDir = movementVec - curDirection;
+        //float change = Mathf.Abs(deltaDir.x) + Mathf.Abs(deltaDir.y);
+        //velocity += 1 / change * accelerationMultiplier;
+        //Debug.Log(deltaDir);
+
+        if (Mathf.Abs(anglediff) < 120)
+        {
+            velocity += (1 - (Mathf.Abs(anglediff) / 180)) * accelerationMultiplier;
+        }
+        else {
+            velocity -= (1 - (Mathf.Abs(anglediff) / 180)) * decelerationMultiplier;
+        }
+        velocity = Mathf.Clamp(velocity, 0, maxVelocity);
+        Debug.Log(anglediff);
 
         // Smoothly interpolate between current velocity and target velocity
-        rb.velocity = Vector2.Lerp(rb.velocity, targetVelocity, smoothTime);
+        rb.velocity = AngleToVector(curAngle + 90).normalized * velocity;
+    }
+
+    Vector2 AngleToVector(float angleDeg) {
+        float rad = angleDeg * Mathf.Deg2Rad;
+
+        return new Vector2(Mathf.Cos(rad), Mathf.Sin(rad));
     }
 }
