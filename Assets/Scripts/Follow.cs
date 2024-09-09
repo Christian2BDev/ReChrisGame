e4e985303fcfca99726d34c246e6e94a1c7d3c5e
Christian2BDev <christiantenbrinke@hotmail.com>
2024-09-09 02:25:21 +0200

    art

---
+++ Assets/Scripts/Follow.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    public GameObject player;
+    [SerializeField]
+    private float speed = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = Vector3.MoveTowards(gameObject.transform.position, player.transform.position, speed * Time.deltaTime);
+        gameObject.transform.position = new Vector3(pos.x, pos.y, -1);
+    }
+}
