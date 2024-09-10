5e314659fdd38c0525e8e48a97dead8d6b5e136e
michiel2005 <60014996+Flawake@users.noreply.github.com>
2024-09-10 11:02:28 +0200

    Improved the boat controls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,7 +16,7 @@ public class PlayerController : MonoBehaviour
     Rigidbody2D rb;
 
     [SerializeField]
-    float maxAngularVelocity = 45f;
+    float angularVelocity = 30f;
 
     [SerializeField]
     float maxVelocity = 1f;
@@ -24,65 +24,44 @@ public class PlayerController : MonoBehaviour
     float minVelocity = -0.3f;
 
     float velocity = 0;
-    // Start is called before the first frame update
+
     void Awake()
     {
         playerReference = this.gameObject;
     }
 
-    void FixedUpdate()
-    {
-        // Get target velocity based on player input
-        Vector2 movementVec = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
-
-        if (movementVec == Vector2.zero) {
-            rb.angularVelocity = 0;
-            return;
-        }
-        float targetAngle = Mathf.Atan2(movementVec.y, movementVec.x) * Mathf.Rad2Deg;
-        targetAngle -= 90;
-
-        float curAngle = transform.eulerAngles.z;
-
-        float anglediff = Mathf.DeltaAngle(curAngle, targetAngle);
-
-        float desiredAngularVelocity = Mathf.Clamp(anglediff, -maxAngularVelocity, maxAngularVelocity);
-
-        rb.angularVelocity = desiredAngularVelocity;
-
-        if (Mathf.Abs(anglediff) < 150)
-        {
-            velocity += (1 - (Mathf.Abs(anglediff) / 180)) * accelerationMultiplier;
-        }
-        else {
-            velocity -= (1 - (Mathf.Abs(anglediff) / 180)) * decelerationMultiplier;
-        }
-        velocity = Mathf.Clamp(velocity, minVelocity, maxVelocity);
-        //Debug.Log(anglediff);
-
-        // Smoothly interpolate between current velocity and target velocity
-        rb.velocity = AngleToVector(curAngle + 90).normalized * velocity;
-    }
-
-    Vector2 AngleToVector(float angleDeg) {
-        float rad = angleDeg * Mathf.Deg2Rad;
-
-        return new Vector2(Mathf.Cos(rad), Mathf.Sin(rad));
-    }
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag.Equals("Enemy"))
+        if(collision.gameObject.CompareTag("Enemy"))
         {
             PlayerStats.ChangeHealth(-10);
             Vector2 enemyVelocity = collision.gameObject.GetComponent<Rigidbody2D>().velocity;
             Vector2 deltaVelocity = enemyVelocity + rb.velocity;
             collision.gameObject.GetComponent<EnemyController>().ChangeHealthAmount(-10);
         }
-        else if (collision.gameObject.tag.Equals("Land"))
+        else if (collision.gameObject.CompareTag("Land"))
         {
             //TODO: take speed and direction into account
             PlayerStats.ChangeHealth(-10);
         }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 movementVec = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
+
+        float curAngle = transform.eulerAngles.z;
+        Vector2 currentMovementVec = AngleToVector(curAngle + 90);
+        velocity += accelerationMultiplier * movementVec.y;
+        velocity = Mathf.Clamp(velocity, minVelocity, maxVelocity);
+        rb.velocity = currentMovementVec * velocity;
+        rb.angularVelocity = -movementVec.x * angularVelocity * velocity;
+    }
+
+    Vector2 AngleToVector(float angleDeg)
+    {
+        float rad = angleDeg * Mathf.Deg2Rad;
+
+        return new Vector2(Mathf.Cos(rad), Mathf.Sin(rad));
     }
 }
